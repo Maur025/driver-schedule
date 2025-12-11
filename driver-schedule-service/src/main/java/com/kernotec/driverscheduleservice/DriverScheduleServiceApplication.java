3960706c50c05d63d9b4cc2fedb9c948d44c96ba
Maur025 <mmoyapalli@gmail.com>
2025-12-11 19:04:23 -0400

    init commit

---
+++ driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/DriverScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kernotec.driverscheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverScheduleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverScheduleServiceApplication.class, args);
+    }
+
+}
