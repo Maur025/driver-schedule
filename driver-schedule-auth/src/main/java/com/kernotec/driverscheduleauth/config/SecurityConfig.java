5f699f7d19e911a25bd7b4539b02458009c8a2f5
Maur025 <mmoyapalli@gmail.com>
2026-01-19 12:25:30 -0400

    feat: reset password from admin

--- driver-schedule-auth/src/main/java/com/kernotec/driverscheduleauth/config/SecurityConfig.java
+++ driver-schedule-auth/src/main/java/com/kernotec/driverscheduleauth/config/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.kernotec.driverscheduleauth.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import com.kernotec.driverscheduleauth.config.bucket.ThrottlingFilter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -26,23 +34,48 @@ public class SecurityConfig {
     private final DriverScheduleAuthProperties driverScheduleAuthProperties;
     private final ThrottlingFilter throttlingFilter;
 
+    private final AccessDeniedHandler webAccessDeniedHandler;
+    private final AuthenticationEntryPoint webAuthenticationEntryPoint;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    @Primary
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ConditionalOnWebApplication(type = Type.SERVLET)
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
             .cors(configurer -> configurer.configurationSource(getCorsConfigurationSource()))
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth.requestMatchers(
                     "/realms/driver-schedule-auth/account/**")
                 .authenticated()
                 .anyRequest()
                 .permitAll())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
             .addFilterBefore(throttlingFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(configurer -> {
+                configurer.authenticationEntryPoint(webAuthenticationEntryPoint);
+                configurer.accessDeniedHandler(webAccessDeniedHandler);
+            })
             .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("");
+
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return jwtAuthenticationConverter;
     }
 
     @Bean
--- driver-schedule-auth/src/main/java/com/kernotec/driverscheduleauth/rest/controller/AccountController.java
+++ driver-schedule-auth/src/main/java/com/kernotec/driverscheduleauth/rest/controller/AccountController.java
@@ -3,11 +3,15 @@ package com.kernotec.driverscheduleauth.rest.controller;
 import com.kernotec.core.rest.dto.response.MessageResponse;
 import com.kernotec.driverscheduleauth.rest.ApiSpec.AccountSpec;
 import com.kernotec.driverscheduleauth.rest.command.account.ProcessAccountChangePasswordCmd;
+import com.kernotec.driverscheduleauth.rest.command.account.ProcessAccountResetPasswordCmd;
 import com.kernotec.driverscheduleauth.rest.dto.request.account.AccountChangePasswordRequest;
+import com.kernotec.driverscheduleauth.rest.dto.request.account.AccountResetPasswordRequest;
 import com.kernotec.driverscheduleauth.util.AuthUtil;
+import com.kernotec.driverscheduleauth.util.RoleUtil.IsRoleAdmin;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Tag(name = AccountSpec.TAG_NAME, description = AccountSpec.TAG_DESCRIPTION)
 @RequestMapping(path = AccountSpec.BASE_PATH)
 @RestController
@@ -24,6 +29,7 @@ public class AccountController {
 
     private final AuthUtil authUtil;
     private final ProcessAccountChangePasswordCmd processAccountChangePasswordCmd;
+    private final ProcessAccountResetPasswordCmd processAccountResetPasswordCmd;
 
     @Operation(summary = "account change password")
     @PostMapping("/password")
@@ -41,6 +47,25 @@ public class AccountController {
         return MessageResponse.builder()
             .code(HttpStatus.OK.value())
             .message("Password changed successfully")
+            .build();
+    }
+
+    @Operation(summary = "account reset password")
+    @PostMapping("/password/reset")
+    @ResponseStatus(HttpStatus.OK)
+    @IsRoleAdmin
+    public MessageResponse resetPassword(@RequestBody AccountResetPasswordRequest request,
+        Authentication authentication)
+    {
+        processAccountResetPasswordCmd.withRequest(ProcessAccountResetPasswordCmd.Request.builder()
+                .accountResetPasswordRequest(request)
+                .adminUserId(authUtil.getUserIdFromAuthentication(authentication))
+                .build())
+            .execute();
+
+        return MessageResponse.builder()
+            .code(HttpStatus.OK.value())
+            .message("Password reset successfully")
             .build();
     }
 }
