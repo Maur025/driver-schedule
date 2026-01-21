92a18c1920ae52106dadc323566e4dc945b4bbfb
Maur025 <mmoyapalli@gmail.com>
2026-01-21 15:59:25 -0400

    feat: Toggle estado enabled/disabled para vehiculos

---
+++ driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/report/rest/command/pdf/PdfExportCmd.java
@@ -0,0 +1,62 @@
+package com.kernotec.driverscheduleservice.report.rest.command.pdf;
+
+import com.kernotec.core.command.AbstractCommand;
+import com.kernotec.driverscheduleservice.report.jpa.enums.ReportDispositionEnum;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.function.Supplier;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PdfExportCmd extends AbstractCommand<PdfExportCmd.Request, Void> {
+
+    @Override
+    protected Void run(Request request) {
+        HttpServletResponse response = request.response;
+        String fileName = Objects.requireNonNullElse(request.fileName, "report")
+            .concat(".pdf");
+
+        byte[] reportPdfBytes = request.callbackGetReportBytes.get();
+
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        response.setHeader(
+            HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.builder(
+                    String.valueOf(request.disposition))
+                .filename(fileName, StandardCharsets.UTF_8)
+                .build()
+                .toString()
+        );
+
+        response.setContentLength(reportPdfBytes.length);
+
+        try (OutputStream out = response.getOutputStream()) {
+            out.write(reportPdfBytes);
+            out.flush();
+        } catch (IOException ex) {
+            log.error("Error while exporting PDF report: {}", ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        }
+
+        return null;
+    }
+
+    @Builder
+    public record Request(@NotNull HttpServletResponse response,
+                          @NotNull Supplier<byte[]> callbackGetReportBytes,
+                          @NotNull ReportDispositionEnum disposition, String fileName)
+    {
+
+    }
+}
