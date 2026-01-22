da3166a27cb69e03e6991ba0baab107306912569
Maur025 <mmoyapalli@gmail.com>
2026-01-22 12:53:57 -0400

    base contact refactor backup

---
+++ driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/jpa/entity/Contact.java
@@ -0,0 +1,43 @@
+package com.kernotec.driverscheduleservice.jpa.entity;
+
+import com.kernotec.core.jpa.entity.BaseAuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "contacts")
+public class Contact extends BaseAuditEntity {
+
+    @Column(name = "label", nullable = false)
+    private String label;
+
+    @Column(name = "value", nullable = false, length = 512)
+    private String value;
+
+    @Column(name = "contact_category_id", nullable = false)
+    private UUID contactCategoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "contact_category_id", referencedColumnName = "id", insertable = false,
+                updatable = false)
+    private ContactCategory contactCategory;
+
+    @Column(name = "person_id", nullable = false)
+    private UUID personId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "person_id", referencedColumnName = "id", insertable = false,
+                updatable = false)
+    private Person person;
+}
--- driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/jpa/entity/Person.java
+++ driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/jpa/entity/Person.java
@@ -1,11 +1,14 @@
 package com.kernotec.driverscheduleservice.jpa.entity;
 
 import com.kernotec.core.jpa.entity.BaseAuditEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.Set;
 import java.util.UUID;
@@ -41,4 +44,7 @@ public class Person extends BaseAuditEntity {
                inverseJoinColumns = @JoinColumn(name = "person_type_id",
                                                 referencedColumnName = "id"))
     private Set<PersonType> personTypes;
+
+    @OneToMany(mappedBy = "person", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Contact> contacts;
 }
