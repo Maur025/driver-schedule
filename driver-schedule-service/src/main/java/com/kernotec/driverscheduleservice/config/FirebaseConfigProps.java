84deb874ca9b5842beca49a2db040c9fbeaa526a
Maur025 <mmoyapalli@gmail.com>
2026-04-27 11:07:13 -0400

    feat: change firebase config to easy deployment

--- driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/config/FirebaseConfigProps.java
+++ driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/config/FirebaseConfigProps.java
@@ -11,5 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 public class FirebaseConfigProps {
 
-    private String keyPath;
+    private String type;
+    private String projectId;
+    private String privateKeyId;
+    private String privateKey;
+    private String client_email;
+    private String clientId;
+    private String authUri;
+    private String tokenUri;
+    private String authProviderX509CertUrl;
+    private String clientX509CertUrl;
+    private String universeDomain;
 }
