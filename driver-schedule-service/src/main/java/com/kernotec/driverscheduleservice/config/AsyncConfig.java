7ff2260465b6be28a4b3ef6e0c9d0230dd94a353
Maur025 <mmoyapalli@gmail.com>
2026-05-04 13:29:32 -0400

    feat: merge functionalities of emergency handled and schedule assignments

--- driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/config/AsyncConfig.java
+++ driver-schedule-service/src/main/java/com/kernotec/driverscheduleservice/config/AsyncConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 @Configuration
 @EnableAsync
@@ -20,6 +21,6 @@ public class AsyncConfig {
         executor.setThreadNamePrefix("FCM-Worker-");
         executor.initialize();
 
-        return executor;
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
     }
 }
